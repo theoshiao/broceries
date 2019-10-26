980a762066e2b055451a4245d697fe7c2d237e11
Theodore Shiao <theo.shiao@gmail.com>
2019-10-26 14:31:01 -0700

    condensed functions

--- prototype.py
+++ prototype.py
@@ -6,9 +6,7 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
-
-def detect_text(path):
-    """Detects text in the file."""
+def itemPriceMapping(path):
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
@@ -18,22 +16,7 @@ def detect_text(path):
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
-
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-
-#         vertices = (['({},{})'.format(vertex.x, vertex.y)
-#                     for vertex in text.bounding_poly.vertices])
-    return texts, response
-        #print('bounds: {}'.format(','.join(vertices)))
-texts, response = detect_text('/Users/theos/Documents/sideprojects/broceries/veggie-grocery-receipt_orig.jpg')
-#texts
-
-unwanted_words = ['safeway', 'mgr', 'manager', 'thank', 'you', 'for', 'shopping', 'with', 'us', 'regprice', 'regular', 
-                 'cardsav', 'resprice', 'scanned', 'coupon', 'ecoupon']
-#parses the text into dictionary of item: price mapping
-def parse_lines(texts):
+    
     stringToParse = texts[0].description
     listOfStrings = []
     listOfItems = []
@@ -48,20 +31,9 @@ def parse_lines(texts):
         if string.startswith('$') or string.startswith('-'):
             listOfPrices.append(string.replace('$', '').replace('-', ''))
     return dict(zip(listOfItems[2:], listOfPrices))
-items = parse_lines(texts)
 
-bros = ['Nilay', 'Theo', 'Anderson', 'Amy']
-itemBro = {'zuchinni green': 'Nilay', 'banana cavendish': 'Nilay', 
-           'special': 'Theo', 'potatoes brushed': 'Anderson', 
-           'broccoli': 'Nilay', 'brussel sprouts': 'Anderson', 
-           'grapes green': 'Amy', 'peas snow': 'Nilay', 
-           'tomatoes grape': 'Anderson', 'lettuce iceberg': 'Theo', 
-           'subtotal': 'None', 'loyalty': 'None', 'total': 'None', 
-           'cash': 'None', 'change': 'None'}
-#itemBro: item: bro mapping
-#items: item: price mapping
-#returns list containing dictionary of item: [price, quantity, bro], subtotal, and total
-def itemsPerBro(itemBro, items):
+
+def finalBroPriceMapping(itemBro, items):
     subtotal = 0
     total = 0
     retItems = {}
@@ -76,20 +48,13 @@ def itemsPerBro(itemBro, items):
             continue
         else:
             retItems[item] = [float(items[item]), 1, itemBro[item]]
-    return [retItems, subtotal, total]
-itemBroMapping = itemsPerBro(itemBro, items)
-itemBroMapping
 
-#people is list of bros, items is dictionary of item:[cost, quantity, bro], 
-def costPerPerson(people, items, subtotal, total):
-    numBros = len(people)
     costPerBro = {}
-    for item in items:
-        if items[item][2] in costPerBro:
-            costPerBro[items[item][2]] += float(items[item][0] * items[item][1])
+    for item in retItems:
+        if retItems[item][2] in costPerBro:
+            costPerBro[retItems[item][2]] += float(retItems[item][0] * retItems[item][1])
         else:
-            costPerBro[items[item][2]] = float(items[item][0] * items[item][1])
+            costPerBro[retItems[item][2]] = float(retItems[item][0] * retItems[item][1])
     for bro in costPerBro:
         costPerBro[bro] = (costPerBro[bro] / subtotal) * total
-        
     return costPerBro;
