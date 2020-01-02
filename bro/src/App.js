544d2d5613de0e0010ea2cfd5d4a74b81c1522bf
Amy Huang <amyxjhuang@berkeley.edu>
2020-01-01 18:23:13 -0800

    fdslkj

--- bro/src/App.js
+++ bro/src/App.js
@@ -19,6 +19,8 @@ class App extends React.Component {
     }
   ],
     data : {
+      "apple": 5.00,
+      "banana": 3.99
     }
 };
 
@@ -41,27 +43,14 @@ class App extends React.Component {
     const axios = require('axios').default;
     var fileObj = this.fileUpload.files[0];
     console.log(fileObj)
-    // fetch('http://127.0.0.1:5000/uploadphoto', {mode: 'no-cors'}, {
-    //   method: 'post',
-    //   body: fileObj
-    // }).then(function(response) {
-    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //   // console.log('body:', body); // Print the HTML for the Google homepage.
-    // }).catch(function(error) {
-    //   console.error('error:', error); // Print the error if one occurred
-    // });
     const formData = new FormData();
     formData.append("file", fileObj);
     axios.post('http://127.0.0.1:5000/uploadphoto', formData).then(function (response) {
       console.log(response.data);
-  }).catch(function (error) {
-    console.log(error);
-  });
-
-    // const request = require('request');
-    // request(, function (error, response, body) {
-    //
-    // });
+      }).catch(function (error) {
+        console.log(error);
+      }
+    );
   }
 
   handleRemoveUser = (id) => {
@@ -102,14 +91,15 @@ class App extends React.Component {
                   ref={(ref) => this.fileUpload = ref} name = "file" />
                 <input onClick={this.imageUpload} type = "submit"/>
 
-             <ItemSelect itemList={this.state.data}
-                         users = {this.state.users}/>
-
              </Col>
 
            </Col>
+
+
           </Row>
         </header>
+        <ItemSelect itemList={this.state.data}
+                    users = {this.state.users}/>
 
       </div>
     )
